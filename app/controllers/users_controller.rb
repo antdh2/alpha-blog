83ed16124be9259de6a2170602732eb97851a23a
Ant Horner <antdh2@gmail.com>
2016-05-22 23:10:46 +0000

    Added redirect for initial signup to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       #do something
       flash[:success] = "Welcome to the alpha blog, #{@user.username}!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       #if there is an error, render new template again
       render 'new'
