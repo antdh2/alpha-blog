a2d6f97e0cd532e702046dc474460907404706d0
antdh2 <antdh2@gmail.com>
2016-05-25 13:31:20 +0000

    Add article and categories association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  # has many article categories
+  has_many :article_categories
+  # has many cetegories
+  has_many :categories, through: article_categories
   
   # rails auto creates getters and setters for articles table columns
   # add validations in the model layer, make sure title is present or DO NOT save
